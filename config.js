04d4e4e922530005963df9a076bd9f6485a076f7
arkanatrisinergi <arkanatrisinergi@gmail.com>
2026-05-16 18:48:53 +0800

    Technical update
    Make the update of database using Apps Script more efficient by having config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+// ═══════════════════════════════════════════════════════════
+// ARKANA APP — Connection Config
+// Update ARKANA_SCRIPT_URL setiap kali deploy Apps Script baru
+// ═══════════════════════════════════════════════════════════
+
+const ARKANA_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzDcMCo1v8G1-8f9cCMk_Ao21sWorg04uD7S_zQemlrhvFjI1iv45TBFcuEU6p8zfPU-w/exec';
